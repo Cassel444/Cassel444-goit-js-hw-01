6d2ba505ef4fc0cc5eeae9e2573290b9c8084897
Cassel444 <chumachocl@gmail.com>
2024-01-28 12:25:28 +0200

    Corrected the mentor's comments

--- js/task-3.js
+++ js/task-3.js
@@ -2,14 +2,13 @@ function getElementWidth(content, padding, border) {
     const numContent = Number.parseFloat(content);
     const numPadding = Number.parseFloat(padding);
     const numBorder = Number.parseFloat(border);
-    let elementWidth = numContent + numPadding * 2 + numBorder * 2;
-    console.log(elementWidth);
+    const elementWidth = numContent + numPadding * 2 + numBorder * 2;
     return elementWidth;
 
 }
-let elementWidth = getElementWidth("50px", "8px", "4px");
-elementWidth = getElementWidth("60px", "12px", "8.5px");
-elementWidth = getElementWidth("200px", "0px", "0px");
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
 
 
 
